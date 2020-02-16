b7ac4d2f11683a8f48d6ca59cbec4e280e483a45
Henrique <henriquethorpeinnovation.com>
2020-02-15 22:34:20 -0300

    Welcome
    This project follow the playlist of Peer Play - https://www.youtube.com/watch?v=4Av788P9stk&list=PL3POsQzaCw53p2tA6AWf7_AWgplskR0Vo&index=2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static float[] _samples = new float[512];
+
+    [SerializeField]
+    AudioSource music;
+
+    [SerializeField]
+    bool playGame;
+
+    [SerializeField]
+    NoteController nController;
+
+    [SerializeField]
+    KeyCode key;
+
+    
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+       
+
+        music.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+
+        /*if (!playGame)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                playGame = true;
+                nController.SetStart(playGame);
+            }
+        }*/
+
+        /*  else if (playGame)
+
+              if (Input.GetKeyDown(key))
+              {
+                  playGame = false;
+                  nController.SetStart(playGame);
+              }*/
+    }
+
+
+}
---
+++ Assets/Scripts/VisualizerRythm.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizerRythm : MonoBehaviour
+{
+    public GameObject _samplesPrefab;
+
+    public GameObject[] _sampleCube = new GameObject[512];
+
+    public float _maxScale;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _instanceCubes = (GameObject)Instantiate(_samplesPrefab);
+            _instanceCubes.transform.position = this.transform.position;
+            _instanceCubes.transform.parent = this.transform;
+            _instanceCubes.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instanceCubes.transform.position = Vector3.forward * 500f;
+            _sampleCube[i] = _instanceCubes;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (_sampleCube != null)
+            {
+                _sampleCube[i].transform.localScale = new Vector3(10, (GameManager._samples[i] * _maxScale) + 2, 10);
+            }
+        }
+    }
+}
